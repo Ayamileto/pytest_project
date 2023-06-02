faa8d7f6c04e4e9a7c93ba0e5c239f22d188fd88
Балахонцева Анастасия <ayamileto@gmail.com>
2023-06-02 21:54:23 +0300

    add tests for 100% coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,6 +6,9 @@ def test_get():
     assert arrs.get([], -1, "test") == "test"
 
 
-def test_slice():
+def test_my_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 0, 0) == []
+    assert arrs.my_slice([1, 2, 3, 4], -1) == [4]
+    assert arrs.my_slice([1, 5, 7], -20) == [1, 5, 7]
