327a2de3d92937ca64cc26d9125c03f78b740b47
Балахонцева Анастасия <ayamileto@gmail.com>
2023-06-05 07:06:44 +0300

    add test for new full func

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pytest_proj.utils import dicts
 
 
@@ -9,3 +11,14 @@ def test_get_val():
     assert dicts.get_val(data, "vcs", "git") == 'mercurial'
     assert dicts.get_val({}, "vcs", "git") == 'git'
     assert dicts.get_val({}, "vcs", "bazaar") == 'bazaar'
+
+
+def test_get_val_collection_type():
+    with pytest.raises(TypeError):
+        dicts.get_val("not a dictionary", "key")
+        dicts.get_val(None, "key")
+
+
+def test_get_val_key_value():
+    with pytest.raises(ValueError):
+        dicts.get_val(data, 123)
